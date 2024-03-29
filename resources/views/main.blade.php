fccbfc59182dfce9160d9e61871958d4b719bff9
Ziaranti07 <ziaranti12@gmail.com>
2024-03-29 15:26:03 +0700

    First commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <title>Ranti Zia - H1101221058</title>
+  @include('layout.link')
+</head>
+
+<body class="g-sidenav-show   bg-gray-100">
+
+   @include('layout.header')
+    @yield('content')
+    @include('layout.footer')
+</body>
+
+@include('layout.script')
+</html>
